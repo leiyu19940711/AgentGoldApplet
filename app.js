40c571e31d6014acbc6074059d3a0ad93fead1d3
leiyu19940711 <50910158+leiyu19940711@users.noreply.github.com>
2020-02-23 22:57:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const WXAPI = require('apifm-wxapi')
 const CONFIG = require('config.js')
-const AUTH = require('utils/auth')
+//const AUTH = require('utils/auth')
 App({
   onLaunch: function() {
-    WXAPI.init(CONFIG.subDomain) // 从根目录的 config.js 文件中读取
+   // WXAPI.init(CONFIG.subDomain) // 从根目录的 config.js 文件中读取
     const that = this;
     // 检测新版本
     const updateManager = wx.getUpdateManager()
